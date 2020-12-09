ea76afdcd2aa738e993309c228f7103fdef94da2
web_XL <597146220@qq.com>
2020-12-09 23:15:14 +0800

    create 完整的 spu

---
+++ src/main/java/io/github/talelin/latticy/dto/SpuDTO.java
@@ -0,0 +1,57 @@
+package io.github.talelin.latticy.dto;
+
+
+import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
+import java.util.List;
+
+@Data
+public class SpuDTO {
+    private String title;
+    private String subtitle;
+    @Positive
+    private Integer categoryId;
+
+//    private Integer rootCategoryId;
+    @Max(1)
+    @Min(0)
+    private Integer online;
+    /**
+     * 文本型价格，有时候SPU需要展示的是一个范围，或者自定义平均价格
+     */
+    @NotBlank
+    @Length(min = 1,max = 20)
+    private String price;
+    /**
+     * 某种规格可以直接附加单品图片
+     */
+    private Integer sketchSpecId;
+    /**
+     * 默认选中的sku
+     */
+    private Integer defaultSkuId;
+    private String img;
+    private String discountPrice;
+    @Length(min = 1,max = 255)
+    private String description;
+
+//    private String tags;
+
+//    private Integer isTest;
+
+//    private String spuThemeImg;
+    private String forThemeImg;
+
+    private List<String> specKeyIdList;
+    private List<String> spuImgList;
+
+    /**
+     * 关联spu_detail_img
+     */
+    private List<String> detailImgs;
+}
